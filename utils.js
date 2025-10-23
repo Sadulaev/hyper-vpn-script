cd6af0be1be2f95e4e4a83fef0bd536b97c4c8b9
Sadulaev <sadulaevismail1999@mail.ru>
2025-10-23 08:24:30 +0300

    updated utils.js

--- utils.js
+++ utils.js
@@ -28,15 +28,50 @@ export function msExpiryEpochPlusOneDay(months) {
 export function chooseBestServer(serversData) {
   if (serversData.length === 0) return null;
 
-  return serversData.reduce((acc, server) => {
-    if (server.currentUsers < server.usersLimit) {
-      if (acc.id && server.currentUsers < acc.currentUsers) {
-        return { id: server.id, currentUsers: server.currentUsers, firstInboundId: server.firstInboundId }; // новый лучший (меньше юзеров и не превышен лимит)
-      }
-    }
-    if (!acc.id || server.currentUsers < acc.currentUsers) {
-      return { id: server.id, currentUsers: server.currentUsers, firstInboundId: server.firstInboundId }; // у всех превышен лимит, выбираем с наименьшим числом юзеров
-    }
-    return acc;
-  }, { id: null, currentUsers: null, firstInboundId: null });
+  console.log(serversData);
+
+  let finalServer = serversData.find((server) => server.currentUsers < server.usersLimit);
+
+  if(!finalServer) {
+    finalServer = serversData.reduce((acc, server) => {
+      if(!acc) return server;
+
+      if(server.currentUsers < acc.currentUsers) return server;
+
+      return acc;
+    }, null)
+  }
+
+  return finalServer;
+
+  // return serversData.reduce((acc, server) => {
+  //   if(acc.id) {
+  //     if(server.currentUsers < server.usersLimit) {
+  //       if(acc.currentUsers >= acc.usersLimit && server.currentUsers < acc.currentUsers) {
+  //         return { id: server.id, currentUsers: server.currentUsers, usersLimit: server.usersLimit, firstInboundId: server.firstInboundId };
+  //       } else {
+  //         return acc;
+  //       }
+  //     } else {
+  //       if(server.currentUsers < acc.currentUsers) {
+  //         return { id: server.id, currentUsers: server.currentUsers, usersLimit: server.usersLimit, firstInboundId: server.firstInboundId };
+  //       } else {
+  //         return acc;
+  //       }
+  //     }
+  //   } else {
+  //     return { id: server.id, currentUsers: server.currentUsers, usersLimit: server.usersLimit, firstInboundId: server.firstInboundId };
+  //   }
+    
+    
+    
+  //   // if (server.currentUsers < server.usersLimit) {
+  //   //   if (acc.id && server.currentUsers < acc.currentUsers) {
+  //   //     return { id: server.id, currentUsers: server.currentUsers, firstInboundId: server.firstInboundId }; // новый лучший (меньше юзеров и не превышен лимит)
+  //   //   }
+  //   // } else if (!acc.id || server.currentUsers < acc.currentUsers) {
+  //   //   return { id: server.id, currentUsers: server.currentUsers, firstInboundId: server.firstInboundId }; // у всех превышен лимит, выбираем с наименьшим числом юзеров
+  //   // }
+  //   // return acc;
+  // }, { id: null, currentUsers: 0, usersLimit: 0, firstInboundId: null });
 };
